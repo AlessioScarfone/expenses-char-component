2fcf1764ddb58495c842d129f001783c56cee72c
Alessio Scarfone <alessioscarfone93@gmail.com>
2022-06-29 08:12:19 +0200

    feat: added TitleCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import styles from "./App.module.scss";
+import { TitleCard } from './TitleCard/TitleCard';
 
 function App() {
   return (
-    <div className="App">
-      prova
+    <div className={styles.App}>
+      <div className={styles.container}>
+
+        <TitleCard title="My balance" balance={921.48}/>
+      </div>
     </div>
   );
 }
---
+++ src/TitleCard/TitleCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './TitleCard.module.scss';
+
+export interface TitleCardProps {
+    title: string,
+    balance: number
+}
+
+export const TitleCard: React.FunctionComponent<TitleCardProps> = ({title, balance}) => {
+  return (
+    <div className={styles.TitleCard}>
+      <div>
+        <h4>{title}</h4>
+        <h2>${balance}</h2>
+      </div>
+      <img alt="logo" src={`${process.env.PUBLIC_URL}/images/logo.svg`} width='20%'/>
+    </div>
+  )
+}
